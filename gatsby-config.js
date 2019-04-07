08645c307da04d6fff00b59730b2243bea70b4e4
Michael Budden <bonjour@immbudden.com>
2019-04-07 02:29:42 -0700

    Sightseeing added - corejs fix v3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,12 @@
 module.exports = {
 
-  siteMetadata: {
-      title: `Buddeen`,
-      siteUrl: `http://buddeen.com`,
-    description: `On 4th July 2019 - Buddeen (Michael and Shereen) get married`,
-    },
-
-    "presets": [
-      [
-        "@babel/preset-env",
-        {
-          "useBuiltIns": "usage",
-          "corejs": 2
-        }
-      ],
-      "@babel/preset-react",
-      "minify"
-    ],
+    siteMetadata: {
+        title: `Buddeen`,
+        siteUrl: `http://buddeen.com`,
+      description: `On 4th July 2019 - Buddeen (Michael and Shereen) get married`,
+      },
 
   plugins: [
-    "@babel/plugin-transform-runtime",
     {
         resolve: `gatsby-source-filesystem`,
         options: {
